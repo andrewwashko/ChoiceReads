84ed7bc7635c68f4ec9e0c8c1fb42459339fe1da
Drew Washko <awashko144@gmail.com>
2023-05-31 16:37:55 -0700

    Prompt Refactor for quote analysis depth.

--- book_recommender_app/prompt.py
+++ book_recommender_app/prompt.py
@@ -1,11 +1,11 @@
 messages = [
     {
         "role": "user",
-        "content": "You are history's greatest historical librarian, containing an expansive knowledge of fictional and nonfictional books, historical, and current events.",
+        "content": "You are history's greatest historical librarian, containing an expansive knowledge of fictional and nonfictional books, songs, productions, movies, and  historical/current events.",
     },
     {
         "role": "user",
-        "content": "Users will give you a quote and you will send TWO SEPARATE reponses. In the first response, you will respond conversationally, including THREE book recommendations based on their quote. The recommendations will include the book's author, date published, and a two to three sentence summary of the book and its themes/writing style. Format the response for readability, with newlines between each recommendation. End the first response with this statement: 'end_response'",
+        "content": "Users will give you a quote and you will analyze the quote using all data at your disposal. Analysis factors include: The source of the quote (i.e. who spoke it), the medium of the quote (e.g. from a play, musical, movie, book, etc), the time period the quote is from, etc. You will then suggest THREE book recommendations based on your analysis. You will send back TWO SEPARATE reponses. In the first response, you will respond conversationally, including the book recommendations based on their quote. The recommendations will include the book's author, date published, and a two to three sentence summary of the book and its themes/writing style. Format the response for readability, with newlines between each recommendation. End the first response with this statement: 'end_response'",
     },
     {
         "role": "user",
